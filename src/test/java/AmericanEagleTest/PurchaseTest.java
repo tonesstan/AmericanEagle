44ce1b9e69f0013d9fb2497d10a318a3bd7febcb
Hooman <tonesstan@gmail.com>
2024-11-21 04:04:02 +0300

    More logs addition and disable test artifacts uploading.

--- src/test/java/AmericanEagleTest/PurchaseTest.java
+++ src/test/java/AmericanEagleTest/PurchaseTest.java
@@ -38,32 +38,38 @@ public class PurchaseTest {
     @Test
     @DisplayName("Покупка случайного товара на American Eagle")
     public void purchase() {
+        System.out.println("Тестируем покупку случайного товара на American Eagle.");
         open("");
 
+        System.out.println("Принимаем куки.");
         $("span[data-test-logo='active-logo-aeo-holiday']").shouldBe(visible);
         $("div#onetrust-banner-sdk").shouldBe(visible);
         $("button#onetrust-accept-btn-handler").shouldBe(visible).click();
         $("div#onetrust-banner-sdk").shouldNotBe(visible);
 
+        System.out.println("\nВыбираем случайную категорию товаров.");
         SelenideElement element = hoverRandomElement($("nav#top-navigation").shouldBe(visible), $("div._mm-top-level_ali1iz"));
         assert element != null;
         clickRandomElement(element, $("a[class*='_column-link_1mj2se']"));
         $("div[data-test-active-logo='aeo']").hover();
 
         scrollToElement($("h1.qa-non-link-label").shouldBe(visible));
+        System.out.println("\nЗашли на страницу выбранной категории товаров.\nВыбираем случайный товар.");
         element = hoverRandomElement($("div.results-list, div.product-list"), $("div.tile-media"));
         assert element != null;
         element.click();
+        System.out.println("Товар выбран.");
 
         $("h1._product-name_kfr7e8").shouldBe(visible);
+        System.out.println("\nОткрыли карточку товара.\nВыбираем доступный размер.");
         scrollToElement($("span.dropdown-text").shouldBe(visible));
         $("span.dropdown-text").click();
         clickRandomElement($("ul.dropdown-menu").shouldBe(visible), $("a:not(:has(small))"));
         $("button.qa-btn-add-to-bag").click();
 
         $x("//h2[text() = 'Added to bag!']").shouldBe(visible);
+        System.out.println("\nВыбранный товар добавлен в корзину.");
         $("button.qa-btn-view-bag").click();
-        System.out.println("Выбранный товар добавлен в корзину.");
 
         $x("//h1[text() = 'Shopping Bag']").shouldBe(visible);
         System.out.println("Зашли на страницу корзины.");
@@ -74,7 +80,7 @@ public class PurchaseTest {
 
         switchTo().window(1);
         $("h1#headerText").shouldBe(visible).shouldHave(text("Pay with PayPal"));
-        System.out.println("Перешли на страницу оплаты PayPal.\nТестируем её компоненты.");
+        System.out.println("Перешли на страницу оплаты PayPal.\nТестируем компоненты страницы.");
         $("#emailSubTagLine").shouldBe(visible).shouldHave(text("Enter your email address to get started."));
         $("#forgotEmail").shouldBe(visible).shouldHave(text("Forgot email?"));
         $("#btnNext").shouldBe(visible).shouldHave(text("Next"));
@@ -85,6 +91,7 @@ public class PurchaseTest {
         switchTo().window(0);
         switchTo().defaultContent();
         scrollToElement($x("//h1[text() = 'Shopping Bag']").shouldBe(visible));
+        System.out.println("Успешно вернулись на страницу корзины.");
 
         System.out.println("Тест прошёл успешно!");
     }
